7868eef564fe7d85b174c8e7d3b6d8751158f09d
FAHRI CIGDEM <64100513+fahricigdem@users.noreply.github.com>
2021-08-29 00:26:51 +0200

    todo app logic

--- src/Todo/App.js
+++ src/Todo/App.js
@@ -1,7 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Container, Input, VStack } from "@chakra-ui/react";
+import { nanoid } from 'nanoid';
+
 const App = () => {
+    ////logic//////////////////////////////
+    const [text, setText] = useState('')
+    const [mytasks, setMytasks] = useState(() => JSON.parse(localStorage.getItem("mytasks")) || [])
+
+    useEffect(() => {
+        localStorage.setItem("mytasks", JSON.stringify(mytasks))
+    }, [mytasks])
+
+    const handleEnter = (e) => {
+        if (e.key === 'Enter') {
+            setMytasks([...mytasks, { "id": nanoid(), "text": text }])
+            setText("")
+        }
+    }
+    const handleDelete = (id) => {
+        setMytasks(mytasks.filter(t => t.id !== id))
+    }
+
+    ////user interface//////////////////////////////
     return (
-        <h1>Todo App</h1>
+        <Container >
+            <VStack m="5" borderColor="red.300" borderWidth='2px'>
+                <h1>Todo App (with localStorage)</h1>
+                <ul>
+                    {mytasks.map(task => <li key={task.id}>{task.text} <span onClick={() => handleDelete(task.id)}>delete</span></li>)}
+                </ul>
+                <Input type="text" value={text} onChange={(e) => setText(e.target.value)} onKeyUp={handleEnter} placeholder="add new task!" />
+            </VStack>
+        </Container>
     );
 }
 
