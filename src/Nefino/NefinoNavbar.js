016b12c014d820c3d4d0559f5848d751a696e322
FAHRI CIGDEM <64100513+fahricigdem@users.noreply.github.com>
2021-08-29 18:01:52 +0200

    navbar background update for md

--- src/Nefino/NefinoNavbar.js
+++ src/Nefino/NefinoNavbar.js
@@ -38,7 +38,7 @@ export default function NefinoNavbar() {
 
   let logo = ""
   {
-    scrolling ? logo = "https://www.nefino.de/wp-content/uploads/2021/02/Nefino_logo.png"
+    (scrolling || isOpen) ? logo = "https://www.nefino.de/wp-content/uploads/2021/02/Nefino_logo.png"
       : logo = "https://www.nefino.de/wp-content/uploads/2021/02/Nefino_white.svg"
   }
 
@@ -46,8 +46,8 @@ export default function NefinoNavbar() {
     <>
 
       <Box px={4} py={3} width="100%" position="fixed" zIndex="1000"
-        color={scrolling && "blue.600"}
-        bg={scrolling && "white"}
+        color={(scrolling || isOpen) && "blue.600"}
+        bg={(scrolling || isOpen) && "white"}
       >
 
         <HStack h={10} >
@@ -94,27 +94,36 @@ export default function NefinoNavbar() {
             aria-label={'Open Menu'}
             display={{ md: 'none' }}
             onClick={isOpen ? onClose : onOpen}
+            _hover={{
+              textDecoration: 'none',
+            }}
+            _active={{
+              textDecoration: 'none',
+            }}
+            _focus={
+              { outLine: "0" }
+            }
           />
           <Text style={{ marginInlineStart: '0' }} display={{ md: 'none' }}>Menü</Text>
 
         </HStack>
 
         {isOpen ? (
-          <Box pb={4} display={{ md: 'none' }} bg="white" color="blue.700" position="absolute" left="0" width="100%" style={{ zIndex: "1" }}>
+          <Box p={4} my={2} display={{ md: 'none' }} bg="white" color="blue.700" position="absolute" left="0" width="100%" style={{ zIndex: "1" }}>
             <Stack as={'nav'} spacing={4}>
               {Links.map((link) => (
                 <NavLink key={link}>{link}</NavLink>
               ))}
               <NavLink>
                 <Image
-                  width="5vw"
+                  width="4vw"
                   height="14px"
                   src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAALCAIAAAD5gJpuAAABLElEQVR4AY2QgUZEQRSGz9ydmzbYkBWABBJYABHEFhJ6m0WP0DMEQNIr9AKrN8ne2Tt3Zs7MOdOZmRBEv+v34Tvub9R6fdNlAzU+snSME/wdjbjbbJ6EiEg6BA8102QbjKNpoMzw8v6qD/sOALbbT2MC1NgaAWOKOgxf5czY+4dbAX2G/THzcozLrvPV85IQyqVz0rvg2p9Pei4HjzSsiFbV4JgyhhxCjpGdZ0RhdikLB9/b8Qig7MkpSovR7Cp59q6CazaNFiTt4J82o6uvdMVwTsztKTXZod4jgOJJuqNAjFyGrBR8gM6XwKfIC4KanBSTZ0rClKh08D9DFh3egW7ebH7NcRDQWrz9rM2Ne+mDOXB2mZJ8agL19nwxR2iZXGm1gDbQKhDjd4yHb2oW/KR8xHicAAAAAElFTkSuQmCC"
                 />
               </NavLink>
               <NavLink>
                 <Image
-                  width="5vw"
+                  width="4vw"
                   height="14px"
                   src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAALCAMAAABBPP0LAAAAmVBMVEViZsViZMJiYrf9gnL8eWrlYkjgYkjZYkj8/PujwPybvPz4+PetraBEgfo+fvo3efkydfkqcvj8Y2T8UlL8Q0P8MzP9k4Hz8/Lu7u4DdPj9/VrKysI9fPoDc/EAZ7z7IiLHYkjp6ekCcOTk5OIASbfY/v21takAJrT5Dg6sYkjc3Nn94t2RkYD+y8KeYkjs/v7l5fz0dF22YkjWvcOLAAAAgElEQVR4AR2KNULFQBgGZ5J13KGGKvc/Cw1uPe62eb9+Jr1EUBFHSgxxjP2Eca6AfUSfVlUfBvm1Ui1bqafctqMndNkXpb01h5TLx4b6TIXgwOCHfjv+/Pz+5vPRw7txGWT2h6yO0/GaYltIp5PT1dEpLNPL/SdWjYjAAZtvRPgHJX4Xio+DSrkAAAAASUVORK5CYII="
                 />
